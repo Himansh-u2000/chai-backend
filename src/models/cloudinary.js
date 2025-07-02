7cd8844b0a7e18084ebc8d97e84b3468d33c7dc4
himansh_u2000 <himanshuhaldar2000@gmail.com>
2025-07-02 23:34:38 +0530

    feat: add user registration functionality with multer and cloudinary integration

---
+++ src/models/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs"
+
+
+const uploadFileOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    });
+    console.log("File Uploaded on Cloudinary successfully : ", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+}
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
